1bdec1b41fd249104a4c5b40eded37355da13b3b
Daniel Amado <danielfep.am@gmail.com>
2020-09-17 13:26:15 -0500

    Typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 int last_digit = n%10;
 
-if(l > 5)
-printf("Last digit of %d is %d an is greater than 5\n", n, l );
-if(l == 0)
-printf("Last digit of %d is %d and is 0\n", n, l);
-else if(l < 6)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+if(last_digit > 5)
+printf("Last digit of %i is %d an is greater than 5\n", n, last_digit);
+if( last_digit == 0)
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
+else if(last_digit < 6)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 return (0);
 }
