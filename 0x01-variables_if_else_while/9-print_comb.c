c7b3589f06317478fa0cbf684b867e17275d89b1
Daniel Amado <danielfep.am@gmail.com>
2020-09-17 20:31:07 -0500

    Program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ char num = '0';
 
 while (num <= '9')
 {
-putchar(num++);
-if(num <= '9')
-putchar(',');
+putchar (num++);
+if (num <= '9')
+{
+putchar (',');
+putchar (' ');
+}
 }
 
 
-putchar('\n');
+putchar ('\n');
 return (0);
 }
