eb98112aa678b6fa8dbb05cf2c14bff0c4dda67a
Daniel Amado <danielfep.am@gmail.com>
2020-09-17 23:33:53 -0500

    P10. 00...99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+unsigned short primero = '0';
+unsigned short segundo;
+
+while (primero <= '9')
+{
+
+for (segundo = '0' ; segundo<='9'; segundo++)
+{
+putchar(primero);
+putchar(segundo);
+
+if ((primero < '9') || (segundo < '9'))
+{
+putchar(',');
+putchar(' ');
+}
+
+}
+
+if (segundo = '9')
+{
+segundo = '0';
+primero++;
+}
+putchar('\n');
+}
+
+return (0);
+}
