5d8affd807f53855bda9a0749f3a46fb22d9d1f6
Daniel Amado <danielfep.am@gmail.com>
2020-09-23 00:30:32 -0500

    Function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-alphabet_x10.c
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -8,8 +8,10 @@
 
 void print_alphabet_x10(void)
 {
+
 char alphabet;
 int i;
+
 for (i = 1; i <= 10; i++)
 {
 
@@ -17,6 +19,7 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
+
 _putchar('\n');
 }
 }
