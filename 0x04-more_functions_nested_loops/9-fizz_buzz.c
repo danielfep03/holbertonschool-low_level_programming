e2180c212d7097ad5ddf33eea565f94287a6755e
Daniel Amado <danielfep.am@gmail.com>
2020-09-25 00:01:46 -0500

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 /**
-* print_square - function that prints a square, followed by a new line
-* @size: size of square
+* main - Fizz Buzz
 *
 */
 
 void main(void)
 {
 int i;
-for(i = 1; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
-if(i % 3 == 0 && i % 5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
 }
-else if(i % 5 == 0)
+else if (i % 5 == 0)
 {
 printf("Buzz ");
 }
-else if(i % 3 == 0)
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
