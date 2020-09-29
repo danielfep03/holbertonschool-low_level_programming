7fab2a6372aa4678c930becb6ec6cce033730c7b
Daniel Amado <danielfep.am@gmail.com>
2020-09-29 02:42:23 -0500

    Function that takes a pointer and updates the value it points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - function that takes a pointer and updates the value it points to 98.
+* *n: pointer
+*
+*/
+
+void reset_to_98(int *n);
+{
+*n = 98;
+}
