4375b1c9e16bde2a826be89c185af3ef5c27eb4b
Daniel Amado <danielfep.am@gmail.com>
2020-09-29 03:17:41 -0500

    function that takes a pointer and updates the value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
-* reset_to_98 - function that takes a pointer and updates the value it points to 98.
-* *n: pointer
+* reset_to_98 - function that takes a pointer and updates the value to 98
+* @n: pointer
 *
 */
 
