50f94bd1163a9099163e6550a48a297c52fb6210
Daniel Amado <danielfep.am@gmail.com>
2020-10-01 18:09:09 -0500

    Function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* _strlen - function that returns the length of a string
+* _strcat - function that concatenates two strings.
 * @dest: string
 * @src: string
 * Return: Length of a string
@@ -10,16 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{}
 
-	for(j = 0; src[j]!='\0'; ++j, ++i)
+	for (j = 0; src[j] != '\0'; ++j, ++i)
 	{
 	dest[i] = src[j];
 	}
 
-	src[i]='\0';
-	return(dest);
+	src[i] = '\0';
+	return (dest);
 }
