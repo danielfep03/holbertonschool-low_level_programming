61f691077b1bcea4a93050c70ddf35fa0c4e5990
Daniel Amado <danielfep.am@gmail.com>
2020-10-03 19:54:31 -0500

    function that prints a string, in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strcmp - function that compares two strings.
+* @a: string
+* @n: string
+* Return: Comparation int
+*/
+
+void reverse_array(int *a, int n)
+{
+	/*int i;
+	char x[];
+
+	for(i = n; i >= 0; i--)
+	{
+		x = a[i];
+	}*/
+	printf("este es n %d", n);
+	printf("este es n %n", a);
+}
