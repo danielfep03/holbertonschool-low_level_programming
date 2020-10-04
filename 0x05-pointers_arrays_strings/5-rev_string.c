6a4eac3ad9afd98db52c1d0e6d6eebfeadd6cf46
Daniel Amado <danielfep.am@gmail.com>
2020-10-03 19:55:53 -0500

    function that prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* rev_string - function that prints a string, in reverse
+* @s: string
+*/
+
+
+void rev_string(char *s)
+{
+	int i, r;
+	char reverse[20] = "";
+
+		for (i = 0; s[i] != '\0'; i++)
+		{}
+
+		i--;
+		for (r = 0; i >= 0; i--, r++)
+		{
+			reverse[r] = s[i];
+		}
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			s[i] = reverse[i];
+		}
+
+}
