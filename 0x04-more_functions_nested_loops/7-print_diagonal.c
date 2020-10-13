1d3e85f6a1820f08a811ad44c07eecf7bf01369f
Daniel Amado <danielfep.am@gmail.com>
2020-10-13 17:14:29 -0500

    Static library

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,35 +1,23 @@
 #include "holberton.h"
 
 /**
-* print_diagonal - function that draws a diagonal line on the terminal
-* @n: number of times the character \ should be printed
+* print_line - function that draws a straight line in the terminal
+*@n: number of times the character _ should be printed
 *
 */
 
 void print_diagonal(int n)
 {
+	int i, x;
 
-int i, x;
-int space = 0;
-
-if (n > 0)
-{
-
-for (i = 1; i <= n; i++)
-{
-space++;
-for (x = 1; x < space; x++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
-
-}
-
-else
-{
-_putchar('\n');
-}
+	for (i = 0; i <= n; i++)
+	{
+		for (x = 0; x <= i; x++)
+		{
+		_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+		_putchar('\n');
 }
