2fcf549fb38b54b408202823d0d6a84c9c471341
Daniel Amado <danielfep.am@gmail.com>
2020-10-15 01:51:53 -0500

    Function that copy the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strdup - Function that copy the string given as a parameter
+ * @str: Size of array to return
+ * Return: Pointer to a newly allocate space in memory
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i]; i++)
+
+		array = malloc(sizeof(char) * i);
+
+		for (i = 0; str[i]; i++)
+		{
+			array[i] = str[i];
+		}
+	}
+	return (array);
+}
