3ac5c9ab612b9433fba7b585b84f77097af7ff4a
Daniel Amado <danielfep.am@gmail.com>
2020-10-15 22:30:48 -0500

    Function that allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - Function that allocates memory
+ * @b: String
+ * Return: Void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit (98);
+	}
+	return (p);
+}
