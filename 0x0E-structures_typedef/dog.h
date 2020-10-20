6bf1e285756b014dbf8383cd598150b3921e20e0
Daniel Amado <danielfep.am@gmail.com>
2020-10-19 22:56:51 -0500

    Struct of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - It´s a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
