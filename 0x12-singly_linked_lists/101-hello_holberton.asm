9c2d1da12d660e1f8bb188ed5040229dac5beca0
Daniel Amado <danielfep.am@gmail.com>
2020-10-31 00:10:46 -0500

    Try New

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,34 @@
-.MODEL SMALL  
-.STACK 100H  
-.DATA  
-  
-;The string to be printed  
-STRING DB 'Hello, Holberton', '$'
-  
-.CODE  
-MAIN PROC FAR  
- MOV AX,@DATA  
- MOV DS,AX  
-  
- ; load address of the string  
- LEA DX,STRING  
-  
- ;output the string 
- ;loaded in dx  
- MOV AH,09H 
- INT 21H  
-  
- ;interrupt to exit
- MOV AH,4CH 
- INT 21H  
-  
-MAIN ENDP  
-END MAIN  
+  global _main
+    extern  _GetStdHandle@4
+    extern  _WriteFile@20
+    extern  _ExitProcess@4
+
+    section .text
+_main:
+    ; DWORD  bytes;    
+    mov     ebp, esp
+    sub     esp, 4
+
+    ; hStdOut = GetstdHandle( STD_OUTPUT_HANDLE)
+    push    -11
+    call    _GetStdHandle@4
+    mov     ebx, eax    
+
+    ; WriteFile( hstdOut, message, length(message), &bytes, 0);
+    push    0
+    lea     eax, [ebp-4]
+    push    eax
+    push    (message_end - message)
+    push    message
+    push    ebx
+    call    _WriteFile@20
+
+    ; ExitProcess(0)
+    push    0
+    call    _ExitProcess@4
+
+    ; never here
+    hlt
+message:
+    db      'Hello, Holberton', 10
+message_end:
