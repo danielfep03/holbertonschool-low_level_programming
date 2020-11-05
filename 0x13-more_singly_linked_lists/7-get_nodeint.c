9b56c3cac759c7ef7a5fc1cf026a65692eacc39c
Daniel Amado <danielfep.am@gmail.com>
2020-11-04 21:57:10 -0500

    Added conditional

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,6 +19,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	store = head;
 
+	if (index == 0)
+	{
+		return (head);
+	}
+
 	for (i = 0; i < index; i++)
 	{
 		store = store->next;
