264719e8ef5169917455c69f28bcbc3f033498f6
Daniel Amado <danielfep.am@gmail.com>
2020-11-04 22:27:24 -0500

    Sum of all the data of a linked list.

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sum of all the data of a linked list.
+ * @head: Head of a list
+ * Return: Sum of all the data (n)
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *store;
+	int add;
+
+	if (!head)
+	{
+		return (0);
+	}
+
+	store = head;
+
+	while (store)
+	{
+		add += store->n;
+		store = store->next;
+	}
+
+	return (add);
+}
