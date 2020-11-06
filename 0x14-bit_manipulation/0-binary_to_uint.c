3f1d006df54000b1a4ad9b35557dd64a69e9c74a
Daniel Amado <danielfep.am@gmail.com>
2020-11-05 22:08:32 -0500

    Return 0 if b is NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,11 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	int decimal = 0, base = 1, num;
 
+	if (!b)
+	{
+		return (0);
+	}
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
