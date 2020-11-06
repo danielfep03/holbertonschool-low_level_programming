1c00e6055282167f62566bd663052c0e0ef5e161
Daniel Amado <danielfep.am@gmail.com>
2020-11-05 22:04:28 -0500

    Converts a binary number to an int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an int
+ * @b:pointing to a string
+ * Return: The converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int decimal = 0, base = 1, num;
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+
+	i--;
+	while (b[i])
+	{
+		num = b[i] - '0';
+		decimal = decimal + num * base;
+		base = base * 2;
+		i--;
+	}
+
+	return (decimal);
+}
