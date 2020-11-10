b836a9c12346caecd3fe8c86e61bd95dc8b232ad
Daniel Amado <danielfep.am@gmail.com>
2020-11-10 17:34:53 -0500

    Betty Style

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file - 
- * @filename: 
- * @text_content:
- * Return: 
+ * append_text_to_file - function that appends text at the end of a file.
+ * @filename: Name of the file
+ * @text_content: String to add at the end of the file
+ * Return: 1 on success and -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
