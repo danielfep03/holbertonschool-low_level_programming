180e0b335697067782d23e5d2f21085203476227
Daniel Amado <danielfep.am@gmail.com>
2020-11-10 14:12:28 -0500

    If text_content is NULL return -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int w, len;
 	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (!filename || fd < 0)
+	if (!filename || fd < 0 || !text_content)
 	{
 		return (-1);
 	}
