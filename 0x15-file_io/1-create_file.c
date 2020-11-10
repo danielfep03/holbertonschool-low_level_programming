6b054feba2fd5c61e37547f66c9ed6fc3e91a7b6
Daniel Amado <danielfep.am@gmail.com>
2020-11-10 16:32:01 -0500

    Write if text_content isn´t NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,20 @@ int create_file(const char *filename, char *text_content)
 	int w, len;
 	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (!filename || fd < 0 || !text_content)
+	if (!filename || fd < 0)
 	{
 		return (-1);
 	}
 
-	for (len = 0; text_content[len]; len++)
-	{}
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+		{}
 
-	w = write(fd, text_content, len);
-	if (w == -1)
-		return (-1);
-
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
+	close(fd);
 	return (1);
 }
