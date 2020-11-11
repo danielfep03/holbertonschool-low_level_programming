8d4dcb28bb295b55398a323b86b876846e8c13a0
Daniel Amado <danielfep.am@gmail.com>
2020-11-11 15:27:55 -0500

    Program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,106 @@
+#include "holberton.h"
+
+char *read_to_file(int fd_from, char *file_from);
+int write_to_file(int fd_to, char *file, char *to);
+
+/**
+ * main - Function that write the content of a file to another file
+ * @ac: Number of arguments
+ * @av: Arguments
+ * Return: 0 on succes
+ */
+
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+	char *file;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage:cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+		if (fd_from < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+	fd_to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+		if (fd_to < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+
+	file = read_to_file(fd_from, av[1]);
+
+	write_to_file(fd_to, file, av[2]);
+
+	if (close(fd_from) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
+ * read_to_file - Read the content of a file
+ * @fd_from: File descriptor
+ * @file_from: Name of the file
+ * Return: A memory space
+ */
+
+
+char *read_to_file(int fd_from, char *file_from)
+{
+	int sz, bytes = 1024;
+	char *file;
+
+	file = malloc(sizeof(char) * bytes);
+
+	if (!file)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	sz = read(fd_from, file, bytes);
+		if (sz == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
+
+	return (file);
+}
+
+/**
+ * write_to_file - Write into a file
+ * @fd_to: File descriptor
+ * @file: Buffer to write
+ * @to: Name of the file
+ * Return:0 On success
+ */
+
+int write_to_file(int fd_to, char *file, char *to)
+{
+	int w;
+	int bytes = 1024;
+
+	w = write(fd_to, file, bytes);
+
+	if (w < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
+		exit(99);
+	}
+	return (0);
+}
