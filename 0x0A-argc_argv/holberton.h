cd2cb436ee581b7f7627a54bdb7903e28a7f008b
Daniel Amado <danielfep.am@gmail.com>
2020-11-29 17:22:45 -0500

    hot fix

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+int main(int argc, char* argv[]);
+
+#endif
