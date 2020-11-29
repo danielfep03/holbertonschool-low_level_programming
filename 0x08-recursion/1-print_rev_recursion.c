85c40029e062054f5b172c81860963cab7e4a0a9
Daniel Amado <danielfep.am@gmail.com>
2020-11-29 15:35:40 -0500

    function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,47 +1,20 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
- * lenght - Return the lenght of a string
- * @s: Stringx
- * Return: Lenght of a string.
- */
-
-int lenght(char *s)
-{
-	if (s[0] == '\0')
-	{
-		return (0);
-	}
-
-	return (1 + lenght(s + 1));
-}
-
- /**
- * prints - Prints a string
- * @s: - String
- * @num: - lenght of string
- */
-
-void prints(char *s, int num)
-{
-	if (num == 0)
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(s[num]);
-	prints(s, num - 1);
-}
-
- /**
- * _print_rev_recursion - Function that prints a string in reverse.
- * @s: String
+ * _print_rev_recursion - function that prints a string in reverse
+ *
+ * @s: string to receive
+ *
+ * Return: nothing
  */
 
 void _print_rev_recursion(char *s)
 {
-	int num;
-
-	num = lenght(s);
-	prints(s, num);	
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		return;
 }
