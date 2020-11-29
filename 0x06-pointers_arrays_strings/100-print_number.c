d4ea2db2199ad37b2dd6021e9a2ebb90d1bc5a78
Daniel Amado <danielfep.am@gmail.com>
2020-11-29 13:58:48 -0500

    Function that prints an integer

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,28 +1,38 @@
 #include "holberton.h"
 
 /**
- * print_number - check the code for Holberton School students.
+ * print_number - Function that prints an integer.
  *
  * @n: Integer.
  */
 
 void print_number(int n)
 {
-	int temp;
-
-	while (n != 0)
-	{
-		if ( n < 100 && n > 9)
+		if (n < 0)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(temp % 10 + '0');
+			_putchar('-');
+			n *= -1;
+		}
+		if (n < 10)
+		{
+			_putchar(n + '0');
+		}
+		else if ( n > 9 && n < 100)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		else if (n >= 100 && n <= 1000)
 		{
-			n = n / 100;
-			_putchar(temp % 10 + '0');
-			n = n / 10;
-			_putchar(temp % 10 + '0');
+			_putchar(n / 100 + '0');
+			_putchar(n / 10 % 10 + '0');
+			_putchar(n % 10 + '0');
 		}
-	}
+		else if (n >= 1000)
+		{
+			_putchar(n / 1000 + '0');
+			_putchar(n / 100 % 10 + '0');
+			_putchar(n % 10 % 10 + '0');
+			_putchar(n % 10 + '0');
+		}
 }
