1988b413b0d083e80654e575891104aadd8240a3
Daniel Amado <danielfep.am@gmail.com>
2020-11-29 22:49:12 -0500

    Hot fix

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,50 +1,41 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * free_grid - function that frees a 2 dimensional grid
- * @grid: the addre
- * @height: height of the grid
- *
- * Return: Nothing.
+ * argstostr - Function that concatenates all the arguments of the program.
+ * @ac: Count.
+ * @av: Vector.
+ * Return: Return an argument.
  */
 char *argstostr(int ac, char **av)
 {
-	char *array;
-	int i, j, k, count;
+	char *p, *q;
+	int i, j, k;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
-
-	for(i = 0; i < ac; i++)
+	for (i = 0, k = 0; i < ac; i++, k++)
 	{
-		for(j = 0;av[i][j]; j++)
+		j = 0;
+		while (*(*(av + i) + j) != '\0')
 		{
-			count++;
-		}
-	}
-
-	printf ("ac + count = %d\n", count + ac);
-
-	array = malloc(sizeof(char *) * ac + count);
-
-	if(!array)
-	{
-		return (NULL);
-	}
-
-	for(i = 1; i < ac; i++)
-	{
-		k++;
-		for(j = 0; av[i][j]; j++, k++)
-		{
-			array[k] = av[i][j];
-			/*printf("%s\n", array);*/
-		}
+			j++;
 			k++;
-			array[k] = '\n';
+		}
 	}
-	/*printf("%s\n", array);*/
-	return(array);
+	k++;
+	p = malloc(k * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	q = p;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*p = av[i][j];
+			p++;
+		}
+		*p = '\n';
+		p++;
+	}
+	return (q);
 }
