1fa1d0a4e9a78f65824fe4474860ca50f008c72b
Daniel Amado <danielfep.am@gmail.com>
2020-12-11 00:55:55 -0500

    First commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Function that prints all the elements of a double list
+ * @h: Head of a doble linked list
+ * Return: Number of elements of the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+	const dlistint_t *temp;
+
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
