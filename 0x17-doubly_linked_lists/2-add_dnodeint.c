250edb45d3d698ace8a1ded58fb83a8ef02a2604
Daniel Amado <danielfep.am@gmail.com>
2020-12-14 00:14:30 -0500

    Betty Style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = *head;
 	new_node->prev = NULL;
 
-	if(*head)
+	if (*head)
 		(*head)->prev = new_node;
 
 	*head = new_node;
