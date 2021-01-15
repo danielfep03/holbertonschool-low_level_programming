3c7413ff19e5458a11fc9a0c605eeeb7a4c46919
Daniel Amado <danielfep.am@gmail.com>
2021-01-15 00:13:30 -0500

    Betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - 
- * @size: 
- * Return: 
+ * hash_table_create - Create a hashtable
+ * @size: size of the array
+ * Return:Pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new;
+	hash_table_t *new;
 
-    if (!size)
-        return (NULL);
+	if (!size)
+		return (NULL);
 
-    new = malloc(sizeof(hash_table_t));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
 
-    new->array = malloc(sizeof(hash_node_t *) * size);
-    if (!new->array)
-    {
-        free(new);
-        return (NULL);
-    }
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new->array)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new->size = size;
-    return (new);
+	new->size = size;
+	return (new);
 }
