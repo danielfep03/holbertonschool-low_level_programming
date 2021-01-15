37f14ac62ddb954471ab503602159c17161f5e8a
Daniel Amado <danielfep.am@gmail.com>
2021-01-15 11:29:39 -0500

    Add key verification

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (!key || !ht || !value)
+	if (!ht || !key || !(*key) || !value)
 		return (0);
 
 	new_node =  malloc(sizeof(hash_node_t));
