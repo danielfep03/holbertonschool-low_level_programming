a1f9e0c422c7cade25b0d1ec0df986a4fc68cbae
Daniel Amado <danielfep.am@gmail.com>
2021-01-15 23:19:46 -0500

    Prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - prints hash table
+ * @ht: Hash a table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	int count = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	i = 0;
+	while (i < ht->size)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (count > 0)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			count++;
+			tmp = tmp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
