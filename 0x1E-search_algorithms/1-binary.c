52f32860e5759a37624b8bab83f0880058815f3a
Daniel Amado <danielfep.am@gmail.com>
2021-04-19 15:28:23 -0500

    Function that search for a value in a sorted array

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * binary_search - Function that search for a value in a sorted array
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in array
+ * @value: Valur to search
+ * Return: Index of value -1 Otherwise
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0, i;
+	int right = size - 1;
+	int middle;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right - 1; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+		middle = floor((left + right) / 2);
+		if (array[middle] < value)
+			left = middle + 1;
+		else if (array[middle] > value)
+			right = middle - 1;
+		else
+			return (middle);
+	}
+	return (-1);
+}
