d3db1beb3d582b305082904422d722a38a1e033c
Daniel Amado <danielfep.am@gmail.com>
2021-04-20 15:41:29 -0500

    [ADDED Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef __SEARCH_ALGOS__
+#define __SEARCH_ALGOS__
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
