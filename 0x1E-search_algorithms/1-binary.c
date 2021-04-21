b14c707e2b992435a05e3970c996f3bdb60b1316
Daniel Amado <danielfep.am@gmail.com>
2021-04-20 19:04:33 -0500

    [UPDATED] Math library removed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 			printf("%d, ", array[i]);
 		}
 		printf("%d\n", array[i]);
-		middle = left + right) / 2;
+		middle = (left + right) / 2;
 		if (array[middle] < value)
 			left = middle + 1;
 		else if (array[middle] > value)
